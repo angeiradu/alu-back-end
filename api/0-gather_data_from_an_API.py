c34ed5e924ae7a7a774dc994cbd9796172da39d5
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-07-04 02:56:21 -0700

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,46 +1,59 @@
 #!/usr/bin/python3
-
-import sys
+"""script that fetches info about a given employee's ID using an api"""
+import json
 import requests
+import sys
 
 
-def get_employee_todo_progress(employee_id):
-    # Construct the URL for the API request
-    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
-
-    # Send a GET request to the API and retrieve the response
-    response = requests.get(url)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the response data as JSON
-        todos = response.json()
-
-        # Get the employee name
-        employee_name = todos[0]['name']
-
-        # Count the number of completed tasks
-        completed_tasks = [todo for todo in todos if todo['completed']]
-        num_completed_tasks = len(completed_tasks)
-
-        # Calculate the total number of tasks
-        total_tasks = len(todos)
-
-        # Display the employee TODO list progress
-        print("Employee {} is done with tasks({}/{}):".format(employee_name, num_completed_tasks, total_tasks))
-        for task in completed_tasks:
-            print("\t{}".format(task['title']))
-    else:
-        print("Error: Failed to retrieve employee TODO list.")
-
+base_url = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == "__main__":
-    # Check if the employee ID was provided as a command-line argument
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-    else:
-        # Get the employee ID from the command-line argument
-        employee_id = int(sys.argv[1])
 
-        # Call the function to get the employee TODO list progress
-        get_employee_todo_progress(employee_id)
+    user_id = sys.argv[1]
+
+    # get user info e.g https://jsonplaceholder.typicode.com/users/1/
+    user_url = '{}/users?id={}'.format(base_url, user_id)
+    # print("user url is: {}".format(user_url))
+
+    # get info from api
+    response = requests.get(user_url)
+    # pull data from api
+    data = response.text
+    # parse the data into JSON format
+    data = json.loads(data)
+    # extract user data, in this case, name of employee
+    name = data[0].get('name')
+    # print("id is: {}".format(user_id))
+    # print("name is: {}".format(name))
+
+    # get user info about todo tasks
+    # e.g https://jsonplaceholder.typicode.com/users/1/todos
+    tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
+    # print("tasks url is: {}".format(tasks_url))
+
+    # get info from api
+    response = requests.get(tasks_url)
+    # pull data from api
+    tasks = response.text
+    # parse the data into JSON format
+    tasks = json.loads(tasks)
+
+    # initialize completed count as 0 and find total number of tasks
+    completed = 0
+    total_tasks = len(tasks)
+
+    # initialize empty list for completed tasks
+    completed_tasks = []
+    # loop through tasks counting number of completed tasks
+    for task in tasks:
+
+        if task.get('completed'):
+            # print("The tasks are: {}\n".format(task))
+            completed_tasks.append(task)
+            completed += 1
+
+    # print the output in the required format
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, completed, total_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task.get('title')))
