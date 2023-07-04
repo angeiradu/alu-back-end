c0ef3ca25ddef75dc3c2b5f63811d585d63cf2c6
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-07-04 03:14:28 -0700

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,39 +4,48 @@ import json
 import requests
 import sys
 
-
 base_url = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python3 script_name.py <user_id>")
+        sys.exit(1)
 
     user_id = sys.argv[1]
 
     # get user info e.g https://jsonplaceholder.typicode.com/users/1/
     user_url = '{}/users?id={}'.format(base_url, user_id)
-    # print("user url is: {}".format(user_url))
 
     # get info from api
     response = requests.get(user_url)
+
+    if response.status_code != 200:
+        print("Error: Failed to fetch user information.")
+        sys.exit(1)
+
     # pull data from api
-    data = response.text
-    # parse the data into JSON format
-    data = json.loads(data)
+    data = response.json()
+
+    if not data:
+        print("Error: No employee found with the provided ID.")
+        sys.exit(1)
+
     # extract user data, in this case, name of employee
     name = data[0].get('name')
-    # print("id is: {}".format(user_id))
-    # print("name is: {}".format(name))
 
     # get user info about todo tasks
     # e.g https://jsonplaceholder.typicode.com/users/1/todos
     tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
-    # print("tasks url is: {}".format(tasks_url))
 
     # get info from api
     response = requests.get(tasks_url)
+
+    if response.status_code != 200:
+        print("Error: Failed to fetch tasks information.")
+        sys.exit(1)
+
     # pull data from api
-    tasks = response.text
-    # parse the data into JSON format
-    tasks = json.loads(tasks)
+    tasks = response.json()
 
     # initialize completed count as 0 and find total number of tasks
     completed = 0
@@ -46,9 +55,7 @@ if __name__ == "__main__":
     completed_tasks = []
     # loop through tasks counting number of completed tasks
     for task in tasks:
-
         if task.get('completed'):
-            # print("The tasks are: {}\n".format(task))
             completed_tasks.append(task)
             completed += 1
 
